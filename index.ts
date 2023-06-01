2c340d0ce440a653161767adbf66a64a321a4114
jk9746987 <jk9746987@gmail.com>
2023-06-01 21:38:42 +0800

    '第四次'

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 interface Person {
-  name: string
-  age: number
+    name: string;
+    age: number;
 }
 
 let tom: Person = {
-  name: 'Tom',
-  age: 25
+    name: 'Tom',
+    age: 25,
 };
 
-function test(){
-    return 'hahaha'
+function test() {
+    return 'hahaha';
 }
